eb58f33fb9b4402ce225336ca5e53623bce6d1e5
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-10-03 17:59:46 -0500

    Initial commit

---
+++ warm-up1.js
@@ -0,0 +1,23 @@
+// Use a loop to print the following to the console:
+// *
+// **
+// ***
+// ****
+// let text = "*";
+// let i = 0;
+// while (i < 5) {
+//   text += "*";
+//   ++i;
+//   console.log(text);
+// }
+
+// let text = "*"
+// for (let i = 0; i < 4; i++) {
+//   text += "*" 
+//   console.log(text);
+// }
+
+
+for (let i = 0; i < 6; i++) {
+  console.log("*".repeat(i+1));
+}
---
+++ warm-up2.js
@@ -0,0 +1,7 @@
+// ---*---
+// --***--
+// -*****-
+
+for (let i = 3, j=1 ; i > 0 ; i--, j+=2) {
+  console.log("-".repeat(i) + "*".repeat(j) + "-".repeat(i));
+}
